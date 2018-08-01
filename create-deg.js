8983d44d44a1b565efc6b4716ec9b836a54e0a47
NiVyAl <nikitavisoc@gmail.com>
2018-08-01 22:01:43 +0300

    1.2 can get degres

--- create-deg.js
+++ create-deg.js
@@ -1,36 +1,34 @@
-$(document).ready(function(){
+var detector = document.querySelector('.detector');
+var grad = new Array();
+var time;
 
-    var detector = document.querySelector('.detector');
-    var grad = new Array();
-    var time;
+for (var i=0; i<180; i++) {
+    grad[i] = document.createElement('div');
+    detector.appendChild(grad[i]);
+    grad[i].classList.add('grad');
+    grad[i].style.cssText = 'transform: rotate(' + i + 'deg)';
+    var reverse = i+180;
+    //grad[i].id = i;
+    //grad[i].value = 'onmouseover="move(' + i + ')';
+    grad[i].innerHTML = '<div class="inner" onmouseover="move(' + i + ')"></div><div class="inner-reverse" onmouseover="move(' + reverse + ')"></div>';
 
-    for (var i=0; i<180; i++) {
-        grad[i] = document.createElement('div');
-        detector.appendChild(grad[i]);
-        grad[i].classList.add('grad');
-        grad[i].style.cssText = 'transform: rotate(' + i + 'deg)';
-        //console.log('before: ' + $(grad[i]).before);
-        //console.log('after: ' + $(grad[i]).after);
-        grad[i].id = i;
+    /*
+    $(grad[i]).hover(function(){
+        time = Date.now();
+        var grad = $(this);
+        console.log(grad);
+        /*
+        console.log('enter: ' + time);
+        console.log('grad[i]: ' + grad[i]); 
 
-        $(grad[i]).hover(function(){
-            time = Date.now();
-            var grad = $(this);
-            console.log(grad);
-            /*
-            console.log('enter: ' + time);
-            console.log('grad[i]: ' + grad[i]); */
-            
-        },function(){
-            time = Date.now() - time;
-            console.log('exit: ' + time);
-        }
-        );
-    };
-    
-    
-    var move = function(deg) {
-        console.log(deg);
+    },function(){
+        time = Date.now() - time;
+        console.log('exit: ' + time);
     }
-    
-});
+    );
+    */
+};
+
+var move = function(deg) {
+    console.log(deg);
+}
