c06d1fee7f234914a0e034f1c7cadb5616110fb7
NiVyAl <nikitavisoc@gmail.com>
2018-08-03 23:19:06 +0300

    1.23 problem with mouseleave

--- create-deg.js
+++ create-deg.js
@@ -8,17 +8,29 @@ for (var i=0; i<180; i++) {
     grad[i].classList.add('grad');
     grad[i].style.cssText = 'transform: rotate(' + i + 'deg)';
     var reverse = i+180;
-    grad[i].innerHTML = '<div class="inner" onmouseenter="move(' + i + ', event)" onmouseleave="move(' + i + ', event)"></div><div class="inner-reverse" onmouseenter="move(' + reverse + ', event)" onmouseleave="move(' + reverse + ', event)"></div>';
+    grad[i].innerHTML = '<div class="inner" onmouseenter="move(' + i + ', event)" onmouseleave="close(' + i + ', event)"></div><div class="inner-reverse" onmouseenter="move(' + reverse + ', event)" onmouseleave="close()"></div>';
 };
 
 var move = function(deg, event) {
-    console.log(deg + ': ' + event.type);
     /*
-    while(event.type == 'mouseenter') {
-        var time = Date.now();
-        while(time > 10) {
-            time = Date.now() - time;
-        };
-        console.log('1s')
-    }*/
-}
+    var intervalId = setInterval(function(){
+        //console.log(event.type);
+        
+        console.log('interval');
+        
+    }, 1000); */
+    if (event.type == 'mouseenter'){
+        console.log('enter');
+    };
+    
+
+};
+
+var close = function(deg, event) {
+    if (event.type == 'mouseleave'){
+        console.log('close');
+    };
+    /*
+    console.log('leave');
+    clearInterval(intervalId); */
+};
