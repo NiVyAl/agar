cfd119c20306a7c8b5a97cb8c793f5c0598219c6
NiVyAl <nikitavisoc@gmail.com>
2018-08-05 11:30:01 +0300

    1.3 движение при наведении

--- create-deg.js
+++ create-deg.js
@@ -1,6 +1,9 @@
 var detector = document.querySelector('.detector');
 var grad = new Array();
 var time;
+var square = document.querySelector('.square');
+var cordinateX = 0;
+var cordinateY = 0;
 
 for (var i=0; i<180; i++) {
     grad[i] = document.createElement('div');
@@ -8,29 +11,26 @@ for (var i=0; i<180; i++) {
     grad[i].classList.add('grad');
     grad[i].style.cssText = 'transform: rotate(' + i + 'deg)';
     var reverse = i+180;
-    grad[i].innerHTML = '<div class="inner" onmouseenter="move(' + i + ', event)" onmouseleave="close(' + i + ', event)"></div><div class="inner-reverse" onmouseenter="move(' + reverse + ', event)" onmouseleave="close()"></div>';
+    grad[i].innerHTML = '<div class="inner" onmouseenter="move(' + i + ', event)" onmouseleave="stop(' + i + ', event)"></div><div class="inner-reverse" onmouseenter="move(' + reverse + ', event)" onmouseleave="stop()"></div>';
 };
+
+var intervalId;
 
 var move = function(deg, event) {
-    /*
-    var intervalId = setInterval(function(){
-        //console.log(event.type);
-        
+    intervalId = setInterval(function(){
         console.log('interval');
+        cordinateX = cordinateX + 1;
+        cordinateY = cordinateY + 1;
         
-    }, 1000); */
-    if (event.type == 'mouseenter'){
-        console.log('enter');
-    };
-    
+        console.log(cordinateY);
+        
+        square.style.transform = 'translate(' + cordinateX + 'px, ' + cordinateY + 'px)';
+    }, 10);    
 
 };
 
-var close = function(deg, event) {
-    if (event.type == 'mouseleave'){
-        console.log('close');
-    };
-    /*
-    console.log('leave');
-    clearInterval(intervalId); */
+var stop = function(deg, event) {
+    console.log('leave')
+    clearInterval(intervalId);
+    console.log(cordinateY);
 };
