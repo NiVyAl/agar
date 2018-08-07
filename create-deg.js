0c58f7d17e37891d93618331295c4976aa12f4be
NiVyAl <nikitavisoc@gmail.com>
2018-08-07 22:46:35 +0300

    1.32 движение работает нормально, но комп тормозит
    radian

--- create-deg.js
+++ create-deg.js
@@ -5,32 +5,47 @@ var square = document.querySelector('.square');
 var cordinateX = 0;
 var cordinateY = 0;
 
+var radian = function(deg) {
+    var rad = (deg * Math.PI)/180;
+    return rad
+};
+
 for (var i=0; i<180; i++) {
     grad[i] = document.createElement('div');
     detector.appendChild(grad[i]);
     grad[i].classList.add('grad');
-    grad[i].style.cssText = 'transform: rotate(' + i + 'deg)';
+    grad[i].style.cssText = 'transform: rotate(' + -i + 'deg)';
     var reverse = i+180;
-    grad[i].innerHTML = '<div class="inner" onmouseenter="move(' + i + ', event)" onmouseleave="stop(' + i + ', event)"></div><div class="inner-reverse" onmouseenter="move(' + reverse + ', event)" onmouseleave="stop()"></div>';
+    grad[i].innerHTML = '<div class="inner" onmouseenter="move(' + radian(reverse) + ', event)" onmouseleave="stop()"></div><div class="inner-reverse" onmouseenter="move(' + radian(i) + ', event)" onmouseleave="stop()"></div>';
 };
 
 var intervalId;
 
 var move = function(deg, event) {
+    var hypothesis = 0;
+    
+    /*
+    console.log('Угол: ' + deg);
+    console.log('cos: ' + Math.cos(deg));
+    console.log('sin: ' + Math.sin(deg)); */
+    
     intervalId = setInterval(function(){
-        console.log('interval');
-        cordinateX = cordinateX + 1;
-        cordinateY = cordinateY + 1;
+        //console.log('interval');
+        hypothesis = hypothesis + 1;
         
-        console.log(cordinateY);
+        cordinateX = (Math.cos(deg) * hypothesis) + cordinateX;
+        var fixCordinateX = -cordinateX
+        cordinateY = (Math.sin(deg) * hypothesis) + cordinateY;
         
-        square.style.transform = 'translate(' + cordinateX + 'px, ' + cordinateY + 'px)';
-    }, 10);    
+        /*console.log('X: ' + cordinateX);
+        console.log('Y: ' + cordinateY); */
+        
+        square.style.transform = 'translate(' + fixCordinateX + 'px, ' + cordinateY + 'px)';
+    }, 100);    
 
 };
 
 var stop = function(deg, event) {
-    console.log('leave')
+    //console.log('leave')
     clearInterval(intervalId);
-    console.log(cordinateY);
 };
